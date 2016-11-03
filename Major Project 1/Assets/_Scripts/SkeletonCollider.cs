ce915da0c9c74bae992a9a2ca946ed02950907b9
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2016-11-03 16:24:05 -0400

    Add to skeleton script to apply upward force when player collides with it

--- Major Project 1/Assets/_Scripts/SkeletonCollider.cs
+++ Major Project 1/Assets/_Scripts/SkeletonCollider.cs
@@ -2,24 +2,35 @@
 using System.Collections;
 using UnityEngine.SceneManagement;
 
-public class SkeletonCollider : MonoBehaviour {
+/*
+   This script is attached to the skelton and applies an upward force
+   when the player collides with it
+*/
 
-	// Use this for initialization
-	void Start () {
-	
+public class SkeletonCollider : MonoBehaviour
+{
+    //skeleton's rigidbody
+    Rigidbody2D rb2d;
+
+    private float velo = 8.0f;
+    private float delay = 1.0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        //get skeleton's rigidbody
+        rb2d = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
 
     void OnCollisionEnter2D(Collision2D coll)
     {
         if (coll.gameObject.CompareTag("Player"))
-        {
-            Destroy(gameObject);
-            SceneManager.LoadScene("DisplayFinalScoreScene");
-        }
+            rb2d.velocity = new Vector2(rb2d.velocity.x, velo);
     }
 }

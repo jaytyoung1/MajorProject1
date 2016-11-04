ec8d603984f95891404f99b960f1fa5e95778865
My ASUS ZenBook Pro <jaytyoung1@gmail.com>
2016-11-04 14:45:05 -0400

    fade out current level text on level start

---
+++ Major Project 1/Assets/_Scripts/TextFade.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TextFade : MonoBehaviour
+{
+    public Text levelText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        StartCoroutine("Fade");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    IEnumerator Fade()
+    {
+        for (float f = 3f; f >= 0; f -= 0.01f)
+        {
+            Color c = levelText.color;
+            //Color c = renderer.material.color;
+            c.a = f;
+            levelText.color = c;
+            yield return null;
+        }
+    }
+}
